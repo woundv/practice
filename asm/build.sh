c3ea615e99844ce433fe78759c21437e4f36595a
wound <153334157+woundv@users.noreply.github.com>
2025-08-27 01:29:08 -0700

    Update build.sh

--- asm/build.sh
+++ asm/build.sh
@@ -1,5 +1,9 @@
 #!/bin/bash
 
+# this file contains the instructions to build the "main.asm" file into an executable
+# name the file youre testing your learning on "main.asm" then build and run tests,
+# afterwards, rename the file to what you desire and move it to the /learned/ directory
+
 nasm -f elf32 main.asm -o main.o &
 
 ld -m elf_i386 main.o -o main
